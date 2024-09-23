11bc2db8943e9164e795dfc9df8834ca7ca54276
Taiyob <oli1412001@gmail.com>
2024-09-23 11:38:25 +0600

    commit

--- src/components/SlotCard.tsx
+++ src/components/SlotCard.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { formatDate } from "../utils/dateFormate";
 
-const ServiceCard = ({ service }) => {
-  const { name, description, price, _id } = service;
-
+const SlotCard = ({ slot }) => {
+  const { date, startTime, endTime } = slot;
   return (
     <div className="relative">
       <div className="border rounded-lg shadow-lg overflow-hidden bg-white transition-transform transform hover:scale-105 hover:shadow-2xl flex flex-col h-full">
@@ -13,11 +13,18 @@ const ServiceCard = ({ service }) => {
           className="w-full h-48 object-cover transition-opacity duration-300 hover:opacity-75"
         />
         <div className="p-4 flex flex-col flex-grow">
-          <h3 className="text-xl font-semibold text-green-700 mb-2">{name}</h3>
-          <p className="text-gray-700 mb-4 flex-grow">{description}</p>
-          <p className="text-lg font-bold text-red-600 mb-4">{price}</p>
+          <h3 className="text-xl font-semibold text-green-700 mb-2">
+            Slot Date: {formatDate(date)}
+          </h3>
+          <p className="text-gray-700 mb-4 flex-grow"></p>
+          <p className="text-lg font-bold text-red-600 mb-4">
+            Slot start: {startTime}
+          </p>
+          <p className="text-lg font-bold text-red-600 mb-4">
+            Slot end: {endTime}
+          </p>
           <Link
-            to={`/slots/${_id}`}
+            to={`/slots`}
             className="bg-gradient-to-r from-blue-500 to-purple-600 text-white font-semibold py-2 px-4 rounded-lg transition duration-300 shadow-md hover:shadow-lg"
           >
             Add to Cart
@@ -28,4 +35,4 @@ const ServiceCard = ({ service }) => {
   );
 };
 
-export default ServiceCard;
+export default SlotCard;
--- src/pages/Slots.tsx
+++ src/pages/Slots.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { useGetSlotByServiceQuery } from "../redux/api/slot/slotApi";
+import { useParams } from "react-router-dom";
+import SlotCard from "../components/SlotCard";
 
 const Slots = () => {
+  const { id } = useParams();
+  const { data } = useGetSlotByServiceQuery(id);
+  console.log(data?.data);
   return (
-    <div>
-      <h1>SLOT</h1>
+    <div className="container">
+      <h1 className="text-4xl font-bold my-10">All Slots</h1>
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10">
+        {data?.data.map((slot) => (
+          <SlotCard key={slot._id} slot={slot} />
+        ))}
+      </div>
     </div>
   );
 };
